3901e7f0a86a1974afed3332bb93aa6585c1837f
Sylvercode <sylver.dev@outlook.com>
2025-10-15 18:57:50 -0400

    Set Outddor Ambiant Light settings base on scene  settings

--- src/ts/ui/render_ambient_light_config_patch.ts
+++ src/ts/ui/render_ambient_light_config_patch.ts
@@ -3,6 +3,8 @@ import type ApplicationV2 from "fvtt-types/src/foundry/client/applications/api/a
 import type { DataField } from "fvtt-types/src/foundry/common/data/fields.mjs";
 import AmbientLightConfig from "fvtt-types/src/foundry/client/applications/sheets/ambient-light-config.mjs";
 import { OutdoorLightFlagNames, OutdoorLightFlags, OutdoorLightFlagsDataModel } from "../data/outdoor_light_flags";
+import applyDefaultOutdoorLightSettings from "../apps/apply_default_outdoor_light_settings";
+import { AmbientLightProxy } from "../proxies/ambient_light_proxy";
 
 /**
  * Iterable of hook definitions for patching the AmbientLightConfig rendering.
@@ -39,6 +41,15 @@ async function renderAmbientLightConfig(
         return;
     }
 
+    if (!content.querySelector('input[name="hidden"]')) {
+        // By default in Foundry V13, there is no input for hidden state of the light.
+        // If its still the case in the future (and no other module add it), we add it here, hidden, 
+        // so that we can control it via our AmbientLightBroker interface.
+        const hiddenFieldGroup = context.document.schema.fields.hidden.toFormGroup({ rootId: context.rootId }, { value: context.document.hidden });
+        hiddenFieldGroup.style.display = "none";
+        content.prepend(hiddenFieldGroup);
+    }
+
     const dataModel = new OutdoorLightFlagsDataModel(context.document);
 
     function toFormGroup(fieldName: keyof OutdoorLightFlags, inputConfig?: { value: any, disabled?: boolean }) {
@@ -58,18 +69,98 @@ async function renderAmbientLightConfig(
         return;
     }
 
+    const ambientLightProxy = new AmbientLightAppConfigProxy(content);
     isOutdoorFieldInput.addEventListener("change", async () => {
         if (!isOutdoorFieldInput.checked
             || context.document.parent === null
         )
             return;
 
-        const maxDarknessInput = content.querySelector('input[name="config.darkness.max"]') as HTMLInputElement | null;
-        if (!maxDarknessInput) {
+        applyDefaultOutdoorLightSettings(ambientLightProxy, context.document.parent);
+    });
+}
+
+/**
+ * Implementation of AmbientLightProxy that manipulates the AmbientLightConfig application UI.
+ */
+class AmbientLightAppConfigProxy implements AmbientLightProxy {
+    constructor(private content: Element) { }
+
+    /**
+     * @inheritdoc
+     */
+    setBright(bright: number) {
+        const brightInput = this.content.querySelector('input[name="config.bright"]') as HTMLInputElement | null;
+        if (!brightInput) {
+            console.error('Could not find input[name="config.bright"]');
+            return;
+        }
+        brightInput.value = String(bright);
+    }
+    /**
+     * @inheritdoc
+     */
+    setDim(dim: number) {
+        const dimInput = this.content.querySelector('input[name="config.dim"]') as HTMLInputElement | null;
+        if (!dimInput) {
+            console.error('Could not find input[name="config.dim"]');
+            return;
+        }
+        dimInput.value = String(dim);
+    }
+    /**
+     * @inheritdoc
+     */
+    getDim(): number {
+        const dimInput = this.content.querySelector('input[name="config.dim"]') as HTMLInputElement | null;
+        if (!dimInput) {
+            console.error('Could not find input[name="config.dim"]');
+            return 0;
+        }
+        return Number(dimInput.value);
+    }
+    /**
+     * @inheritdoc
+     */
+    setHidden(hidden: boolean) {
+        const hiddenInput = this.content.querySelector('input[name="hidden"]') as HTMLInputElement | null;
+        if (!hiddenInput) {
+            console.error('Could not find input[name="hidden"]');
+            return;
+        }
+        hiddenInput.checked = hidden;
+    }
+    /**
+     * @inheritdoc
+     */
+    setLuminosity(luminosity: number) {
+        const luminosityInput = this.content.querySelector('range-picker[name="config.luminosity"]') as HTMLInputElement | null;
+        if (!luminosityInput) {
+            console.error('Could not find range-picker[name="config.luminosity"]');
+            return;
+        }
+        luminosityInput.value = String(luminosity);
+    }
+    /**
+     * @inheritdoc
+     */
+    setDarknessMax(max: number) {
+        const darknessMaxInput = this.content.querySelector('input[name="config.darkness.max"]') as HTMLInputElement | null;
+        if (!darknessMaxInput) {
             console.error('Could not find input[name="config.darkness.max"]');
             return;
         }
-
-        maxDarknessInput.value = String(context.document.parent.environment.globalLight.darkness.max);
-    });
+        darknessMaxInput.value = String(max);
+    }
+    /**
+     * @inheritdoc
+     */
+    setAttenuation(attenuation: number) {
+        const attenuationInput = this.content.querySelector('range-picker[name="config.attenuation"]') as HTMLInputElement | null;
+        if (!attenuationInput) {
+            console.error('Could not find range-picker[name="config.attenuation"]');
+            return;
+        }
+        attenuationInput.value = String(attenuation);
+    }
 }
